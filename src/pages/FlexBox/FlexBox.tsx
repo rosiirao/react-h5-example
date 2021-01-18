7526bd79f531d7dab2967a2e4d2b8e7182b86fb7
rosiirao <raoyu2009@yahoo.com>
2021-01-18 19:34:38 +0800

    feat!: add flex box examples.

---
+++ src/pages/FlexBox/FlexBox.tsx
@@ -0,0 +1,20 @@
+import "./FlexBox.scss";
+
+function FlexBox() {
+  const contents = ["one item", "two item", "three item"];
+  return (
+    <div className="flex-box">
+      {contents.map((c, i) => (
+        <div key={i} className="flex-box__item flex-box__item--33">
+          {c}
+        </div>
+      ))}
+      <div className="flex-box__item flex-box__item--66 flex-box__item--80--large">
+        four item
+      </div>
+      <div className="flex-box__item flex-box__item--33">five item</div>
+    </div>
+  );
+}
+
+export default FlexBox;
